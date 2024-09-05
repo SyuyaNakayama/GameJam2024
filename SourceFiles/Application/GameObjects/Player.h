49da90aa1c553f655767e24a59414d07313b9ef7
SyuyaNakayama <156877387+Jysterjyn@users.noreply.github.com>
2024-09-05 10:39:50 +0900

    エラー治ったかな？

--- SourceFiles/Application/GameObjects/Player.h
+++ SourceFiles/Application/GameObjects/Player.h
@@ -2,11 +2,11 @@
 #include "GameObject.h"
 #include "Sprite.h"
 
-class Player : public WristerEngine::_3D::GameObject
+class Player : public WristerEngine::_2D::GameObject
 {
 	std::unique_ptr<WristerEngine::_2D::Sprite> playerSprite;
 
 	// GameObject ‚ğ‰î‚µ‚ÄŒp³‚³‚ê‚Ü‚µ‚½
-	void Initialize(const ObjectData& objectData) override;
+	void Initialize() override;
 	void Update() override;
 };
--- SourceFiles/Engine/GameObject/GameObject.h
+++ SourceFiles/Engine/GameObject/GameObject.h
@@ -4,26 +4,53 @@
 #include "SceneManager.h"
 #include "Constant.h"
 
-namespace WristerEngine::_3D
+namespace WristerEngine
 {
-	// ゲームに表示されている3Dオブジェクトの基底クラス
-	class GameObject
+	namespace _2D
 	{
-	protected:
-		Input* input = Input::GetInstance();
-		Constant* constant = Constant::GetInstance();
+		// ゲームに表示されている2Dスプライトの基底クラス
+		class GameObject
+		{
+		protected:
+			Input* input = Input::GetInstance();
+			Constant* constant = Constant::GetInstance();
 
-	public:
-		// 仮想デストラクタ
-		virtual ~GameObject() = default;
-		/// <summary>
-		/// 初期化(純粋仮想関数)
-		/// </summary>
-		/// <param name="objectData">Jsonファイルから読み込んだデータ</param>
-		virtual void Initialize(const ObjectData& objectData) = 0;
-		// 更新(純粋仮想関数)
-		virtual void Update() = 0;
-		// 現在のシーンを取得
-		Scene GetNowScene() const { return SceneManager::GetInstance()->GetNowScene(); }
-	};
+		public:
+			// 仮想デストラクタ
+			virtual ~GameObject() = default;
+			/// <summary>
+			/// 初期化(純粋仮想関数)
+			/// </summary>
+			/// <param name="objectData">Jsonファイルから読み込んだデータ</param>
+			virtual void Initialize() = 0;
+			// 更新(純粋仮想関数)
+			virtual void Update() = 0;
+			// 現在のシーンを取得
+			Scene GetNowScene() const { return SceneManager::GetInstance()->GetNowScene(); }
+		};
+	}
+
+	namespace _3D
+	{
+		// ゲームに表示されている3Dオブジェクトの基底クラス
+		class GameObject
+		{
+		protected:
+			Input* input = Input::GetInstance();
+			Constant* constant = Constant::GetInstance();
+
+		public:
+			// 仮想デストラクタ
+			virtual ~GameObject() = default;
+			/// <summary>
+			/// 初期化(純粋仮想関数)
+			/// </summary>
+			/// <param name="objectData">Jsonファイルから読み込んだデータ</param>
+			virtual void Initialize(const ObjectData& objectData) = 0;
+			// 更新(純粋仮想関数)
+			virtual void Update() = 0;
+			// 現在のシーンを取得
+			Scene GetNowScene() const { return SceneManager::GetInstance()->GetNowScene(); }
+		};
+	}
 }
