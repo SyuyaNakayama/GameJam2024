4cf903571232b28640cb5d66553a470e49b07b87
NishizawaYuga <k021g1246@g.neec.ac.jp>
2024-09-13 14:56:54 +0900

    キャラとロゴ配置、疑似イージング

--- SourceFiles/Application/GameScenes/TitleScene.cpp
+++ SourceFiles/Application/GameScenes/TitleScene.cpp
@@ -8,6 +8,22 @@ void TitleScene::Initialize()
 	// UI•`‰æƒNƒ‰ƒX‚Ì‰Šú‰»
 	uiDrawer = std::make_unique<UIDrawerTitleScene>();
 	uiDrawer->Initialize();
+
+	//”wŒi
+	bg = Sprite::Create("title_background.png");
+	//ƒLƒƒƒ‰
+	player = Sprite::Create("Player/drill_girl.png");
+	player->size = WEConst(Vector2, "EffectSize");
+	player->SetAnimation(4, 60);
+	player->position = { 200,WristerEngine::WIN_SIZE.y - WEConst(float, "GroundHeight") };
+	player->anchorPoint = { -0.5f,1.0f };
+	//ƒƒS
+	logo = Sprite::Create("UI/logo.png");
+	logo->position = { WristerEngine::WIN_SIZE.x / 2,WEConst(float, "GroundHeight") / 2 };
+	logo->size = WEConst(Vector2, "LogoSize");
+
+	chargeMove = 0.5f;
+	isUDChange = false;
 }
 
 void TitleScene::Update()
@@ -15,8 +31,32 @@ void TitleScene::Update()
 	if (operate->GetTrigger("SceneChange")) {
 		sceneManager->ChangeScene(Scene::Tutorial);
 	}
+	logo->position.y += chargeMove;
+	if (!isUDChange) {
+		chargeMove -= 0.01f;
+	}
+	else {
+		chargeMove += 0.01f;
+	}
+	if (chargeMove < -0.5f) {
+		isUDChange = true;
+	}
+	else if (chargeMove > 0.5f) {
+		isUDChange = false;
+	}
+
 	// UI•`‰æ
-	uiDrawer->Update();
+	//uiDrawer->Update();
+	//ƒAƒjƒ[ƒVƒ‡ƒ“•`‰æ
+	bg->Update();
+	player->Update();
+	logo->Update();
+}
+
+void TitleScene::Draw() {
+	bg->Draw();
+	player->Draw();
+	logo->Draw();
 }
 
 void UIDrawerTitleScene::Initialize()
--- SourceFiles/Application/GameScenes/TitleScene.h
+++ SourceFiles/Application/GameScenes/TitleScene.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "BaseScene.h"
 #include "AbstractUIDrawer.h"
+#include "Easing.h"
 
 // UI描画クラス(TitleScene用)
 class UIDrawerTitleScene : public WristerEngine::_2D::AbstractUIDrawer
@@ -15,6 +16,16 @@ class TitleScene : public WristerEngine::BaseScene
 	// BaseScene を介して継承されました
 	void Initialize() override;
 	void Update() override;
+	
+	void Draw() override;
 
 	OperateConfig* operate = OperateConfig::GetInstance();
+
+	float chargeMove;
+	bool isUDChange;
+
+	//タイトル用スプライト
+	std::unique_ptr<WristerEngine::_2D::Sprite> bg;
+	std::unique_ptr<WristerEngine::_2D::Sprite> player;
+	std::unique_ptr<WristerEngine::_2D::Sprite> logo;
 };
